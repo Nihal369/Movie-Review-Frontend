5611600d7d720def2c9ae6ede6f36066511b198b
Nihal369 <nihalkatteripc@gmail.com>
2026-05-21 23:57:38 +0530

    frontend

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000/api";
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
 export const getMovies = async (
   page = 1
